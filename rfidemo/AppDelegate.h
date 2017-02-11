e8bc81c9f4df88d8a40ccd7193920cd01bbd6d73
Kirill Sidorov <web.sidorov@gmail.com>
2017-02-11 11:34:05 +0700

    Initial Commit

---
+++ rfidemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rfidemo
+//
+//  Created by Кирилл on 11.02.17.
+//  Copyright © 2017 Кирилл Сидоров. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rfidemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  rfidemo
+//
+//  Created by Кирилл on 11.02.17.
+//  Copyright © 2017 Кирилл Сидоров. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
